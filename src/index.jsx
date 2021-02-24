672c02d21b632bc9ee195b72da1dc7e057236292
Chris Gitmed <cgitmed@gmail.com>
2021-02-23 22:19:43 -0800

    Added folder for components.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PageContainer from './page-container';
+import PageContainer from './components/page-container';
 
 ReactDOM.render(
     <PageContainer />,
