d26d3e140e11844f7efae36f58eacdc0abb93370
Chris Gitmed <chris@Chriss-MacBook-Air.local>
2021-02-23 21:28:44 -0800

    Added a Title component

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PageContainer from './page-container';
+
+ReactDOM.render(
+    <PageContainer />,
+    document.querySelector('#root')
+)
---
+++ src/page-container.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Title from './title';
+
+export default function PageContainer() {
+    return (
+        <div className="page-container">
+            <Title />
+        </div>
+    )
+}
