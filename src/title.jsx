12a9a72e92683e1f8596562f43e0b37d9d4b81cb
Chris Gitmed <chris@Chriss-MacBook-Air.local>
2021-02-23 21:52:30 -0800

    Added function for converting the day number into a string.

--- src/title.jsx
+++ src/title.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
 export default function Title() {
+
+    function getDayInString(dayInNum) {
+        switch (dayInNum) {
+            case 0:
+                return 'Sunday';
+            case 1:
+                return 'Monday';
+            case 2: 
+                return 'Tuesday';
+            case 3: 
+                return 'Wednesday';
+            case 4:
+                return 'Thursday';
+            case 5:
+                return 'Friday';
+            case 6: 
+                return 'Saturday';
+        }
+    }
+
+    const date = new Date();
+    const day = getDayInString(date.getDay());
+    
+    const today = `${day}, ${date.getMonth()} ${date.getDate()}`
     return (
-        <h1 className="title">Stand-Up!</h1>
+        <div>
+            <h1 className="title">Stand-Up! {today}</h1>
+        </div>
     )
 }
