52cdc394a8693aca67c11fbc0b0e14eaa289bde3
Chris Gitmed <cgitmed@gmail.com>
2021-02-23 23:32:50 -0800

    Added key prop to the returned li elements.

--- src/components/shuffler.jsx
+++ src/components/shuffler.jsx
@@ -20,9 +20,9 @@ export default function Shuffler() {
             setName('');
         }
     }
-    
+
     const listItems = list.map((item, index) => {
-        return <li>{item}</li>
+        return <li key={index}>{item}</li>
     })
 
     return (
