811dda51f6878bd39cc2d34a30025377f077f3c6
Chris Gitmed <chris@Chriss-MacBook-Air.local>
2021-02-23 21:58:33 -0800

    Added a function to convert the month into a string

--- src/title.jsx
+++ src/title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Title() {
 
     function getDayInString(dayInNum) {
-        switch (dayInNum) {
+        switch(dayInNum) {
             case 0:
                 return 'Sunday';
             case 1:
@@ -21,10 +21,38 @@ export default function Title() {
         }
     }
 
+    function getMonthInString(monthInNum) {
+        switch(monthInNum) {
+            case 0:
+                return 'Jan';
+            case 1:
+                return 'Feb';
+            case 2:
+                return 'Mar';
+            case 3:
+                return 'Apr';
+            case 4:
+                return 'May';
+            case 5:
+                return 'June';
+            case 6:
+                return 'July';
+            case 7:
+                return 'Aug';
+            case 8:
+                return 'Sep';
+            case 9:
+                return 'Oct';
+            case 10:
+                return 'Nov';
+            case 11:
+                return 'Dec';
+        }
+    }
     const date = new Date();
     const day = getDayInString(date.getDay());
-    
-    const today = `${day}, ${date.getMonth()} ${date.getDate()}`
+    const month = getMonthInString(date.getMonth());
+    const today = `${day}, ${month} ${date.getDate()}`
     return (
         <div>
             <h1 className="title">Stand-Up! {today}</h1>
