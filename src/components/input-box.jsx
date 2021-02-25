7e04397de972d403702075c1e9865a9a82705e86
Chris Gitmed <cgitmed@gmail.com>
2021-02-24 19:43:14 -0800

    Added an icon for the post button.

--- src/components/input-box.jsx
+++ src/components/input-box.jsx
@@ -5,7 +5,7 @@ export default function InputBox({ name, setName, handlePostClick, handleKeyDown
     return (
         <div className="input-box">
             <input type="text" value={name} onChange={setName} onKeyDown={handleKeyDown}/>
-            <button className="green-button" onClick={handlePostClick}>+</button>
+            <button className="green-button" onClick={handlePostClick}><span className="lnr lnr-plus-circle center"></span></button>
             <button className="shuffle-button" onClick={handleShuffleClick}>Shuffle</button>
         </div>
     )
